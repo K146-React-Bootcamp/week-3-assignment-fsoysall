80b0b8920d9165f92cba3eb7f78f32d966f32b03
Fatih SOYSAL <106076235+fsoysall@users.noreply.github.com>
2022-10-15 01:43:58 +0300

    ALL: OK ( DAY1 , Dark-Light , Custom Event )

---
+++ day1/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/header";
+import TodoList from "./components/todo-list/index-todo-list";
+import  "./App.css"
+// import ContactForm from "./components/contact-form";
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      {/* <ContactForm /> */}
+      <TodoList />
+    </div>
+  );
+}
